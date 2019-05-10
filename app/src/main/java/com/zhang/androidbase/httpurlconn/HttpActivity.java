4c7dda19ce69512b8ccd5b8a88f559383f67ac03
MengkZhang <653651979@qq.com>
2019-05-10 18:04:05 +0800

    httpURLConnection请求网络图片&&不依赖图片框架显示在ImageView&&缓存图片代码

--- app/src/main/java/com/zhang/androidbase/httpurlconn/HttpActivity.java
+++ app/src/main/java/com/zhang/androidbase/httpurlconn/HttpActivity.java
@@ -1,15 +1,22 @@
 package com.zhang.androidbase.httpurlconn;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.zhang.androidbase.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -19,10 +26,13 @@ import java.net.URLConnection;
 
 public class HttpActivity extends AppCompatActivity {
 
+    ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http);
+        mImageView = findViewById(R.id.iv_net_pic_show_without_glide);
     }
 
     @SuppressLint("HandlerLeak")
@@ -40,6 +50,12 @@ public class HttpActivity extends AppCompatActivity {
                     break;
                 case 3://异常
                     Toast.makeText(HttpActivity.this, "异常", Toast.LENGTH_SHORT).show();
+                    break;
+
+
+                case 4://获取显示bitmap
+                    Bitmap bitmap = (Bitmap) msg.obj;
+                    mImageView.setImageBitmap(bitmap);
                     break;
             }
         }
@@ -94,5 +110,65 @@ public class HttpActivity extends AppCompatActivity {
             }
         }).start();
 
+    }
+
+    public void reqHttpWebPicShow(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String path = "https://upload-images.jianshu.io/upload_images/5914881-8e0f627c5963bd0d.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1000/format/webp";
+                File  file = new File(getCacheDir(),Base64.encodeToString(path.getBytes(), Base64.DEFAULT));
+
+                //使用缓存数据
+                if (file.exists() && file.length() >0) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                    Message message = mHandler.obtainMessage();
+                    message.obj = bitmap;
+                    message.what = 4;
+                    mHandler.sendMessage(message);
+                    return;
+                }
+                //请求网络数据
+                try {
+                    URL url = new URL(path);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+                    int responseCode = httpURLConnection.getResponseCode();
+                    if (responseCode == 200) {
+                        InputStream inputStream = httpURLConnection.getInputStream();
+                        FileOutputStream fos = new FileOutputStream(file);
+                        int len = -1;
+                        byte[] buffer = new byte[1024];
+                        while ((len = inputStream.read(buffer)) != -1) {
+                            fos.write(buffer,0,len);
+                        }
+                        fos.close();
+                        inputStream.close();
+
+                        final Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+
+                        Message message = mHandler.obtainMessage();
+                        message.obj = bitmap;
+                        message.what = 4;
+                        mHandler.sendMessage(message);
+
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                mImageView.setImageBitmap(bitmap);
+//                            }
+//                        });
+
+                    } else {
+
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
